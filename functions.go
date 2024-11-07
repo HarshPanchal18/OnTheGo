ec0ab15fc5cde591047fea4827b0107ef0bfe0e7
Harsh Panchal <harshhhh1803@gmail.com>
2024-11-07 13:55:41 +0530

    Created functions

---
+++ functions.go
@@ -0,0 +1,45 @@
+package main
+
+func average(nums []float64) float64 {
+	// panic("Not implemented")
+	total := 0.0
+
+	for _, v := range nums {
+		total += v
+	}
+
+	return total / float64(len(nums))
+}
+
+// Returning multiple values
+func f() (int, int) {
+	return 5, 6
+}
+
+// Variadic functions
+func add(arguments ...int) int {
+	total := 0
+	for _, v := range arguments {
+		total += v
+	}
+	return total
+}
+
+func evenGenerator() func() int {
+	i := int(0)
+
+	return func() (value int) { // parameter signature should be match with parent method signature.
+		value = i
+		i += 2
+		return // `value` is returned here.
+	}
+}
+
+// Recursion
+func factorial(x uint) uint {
+	if x == 0 {
+		return 1
+	}
+
+	return x * factorial(x-1)
+}
--- hello.go
+++ hello.go
@@ -13,5 +13,28 @@ func main() {
 
 	// loops()
 
-	collections()
+	// collections()
+
+	println(average([]float64{1.0, 2.0, 3.0}))
+
+	x,y := f()
+	println(x, y)
+
+	println(add(1,6,3))
+	println(add([]int{1,6,3,8}...))
+
+	// Closure
+	add := func(x, y int) int {
+		return x + y
+	}
+
+	println(add(1,5))
+
+	nextEven := evenGenerator()
+	println(nextEven())
+	println(nextEven())
+	println(nextEven())
+
+	println(factorial(5))
+
 }
