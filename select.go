01b2d5a793e32f197b8c82554af4ad20e575096e
Harsh Panchal <harshhhh1803@gmail.com>
2024-11-11 19:47:58 +0530

    Implemented timeout

--- select.go
+++ select.go
@@ -11,6 +11,7 @@ This function prints “from 1” every 2 seconds and “from 2” every 3 secon
 `select` picks the first channel that is ready and receives from it (or sends to it).
 If more than one of the channels are ready then it randomly picks which one to receive from.
 If none of the channels are ready, the statement blocks until one becomes available.
+The select statement is often used to implement a timeout.
 */
 
 func selections() {
@@ -39,6 +40,14 @@ func selections() {
 
 			case msg2 := <- c2:
 				fmt.Println(msg2)
+
+			// `time.After`` creates a channel and after the given duration will send the current time on it.
+			case <- time.After(time.Second):
+				fmt.Println("timeout...")
+
+			/*default: // happens immediately if none of the channels are ready.
+				fmt.Println("Nothing ready!")*/
+
 			}
 		}
 	}()
