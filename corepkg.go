6a7794ce01c34114a90dc55d0fb4d7e4f022b998
Harsh Panchal <harshhhh1803@gmail.com>
2024-11-14 18:08:17 +0530

    Read file function

--- corepkg.go
+++ corepkg.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
 	"strings"
 )
@@ -55,4 +56,32 @@ func ospkg() {
 	// Fetch ENV variables through a key.
 	fmt.Println(strings.Split(os.Getenv("path"),";"))
 
+	er := os.WriteFile("test1.txt",[]byte {}, fs.ModeAppend)
+	if er != nil {
+		fmt.Println("Error creating test file.")
+		return
+	}
+
+	fmt.Println(getFileContent("test.txt"))
+}
+
+
+func getFileContent(filename string) string {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error opening file ", filename)
+		return err.Error()
+	}
+
+	defer file.Close()
+
+	stat, err := file.Stat()
+	if err != nil { return err.Error() }
+
+	totalBytes := make([]byte, stat.Size())
+	_, err = file.Read(totalBytes)
+	if err != nil { return err.Error() }
+
+	content := string(totalBytes)
+	return content
 }
