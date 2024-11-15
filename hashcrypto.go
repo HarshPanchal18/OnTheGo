a754f874f686d365062de99597953d506ed08e83
Harsh Panchal <harshhhh1803@gmail.com>
2024-11-15 12:27:01 +0530

    Implemented SHA1 hashing

--- hashcrypto.go
+++ hashcrypto.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"crypto/sha1"
 	"hash/crc32"
 	"os"
 )
 
 func hashedBytes(bytes []byte) uint32 {
-	h := crc32.NewIEEE()
-	h.Write(bytes)
-	v := h.Sum32()
-	return v
+	hash := crc32.NewIEEE()
+	hash.Write(bytes)
+	value := hash.Sum32()
+	return value
 }
 
 func getHash(filename string) (uint32, error) {
 	bytes, err := os.ReadFile(filename)
 	if err != nil { return 0, err }
 	return hashedBytes(bytes), nil
+}
+
+func sha1Hash(bytes []byte) []byte {
+	hash := sha1.New()
+	hash.Write(bytes)
+	bytes = hash.Sum([]byte{})
+	return bytes
 }
--- hello.go
+++ hello.go
@@ -3,14 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(hashedBytes([]byte("test")))
-	fmt.Println(hashedBytes([]byte("test2")))
-
-	hash1, err := getHash("test.txt")
-	if err != nil { return }
-
-	hash2, err := getHash("test2.txt")
-	if err != nil { return }
-
-	fmt.Println(hash1, hash2, hash1 == hash2)
+	fmt.Println(string(sha1Hash([]byte("foo"))))
 }
